c528a8395c9a9f7233060812a174856dca74e8bc
saran-gangster <saran.r@ramanaschool.com>
2025-09-30 14:57:47 +0530

    Scale up

--- src/train.py
+++ src/train.py
@@ -1,7 +1,8 @@
 from lightning.pytorch.cli import LightningCLI
 from lightning.pytorch.loggers import TensorBoardLogger, WandbLogger
 from src.lightning.lit_jepa import LitJEPA
-from src.data.tiny_shakespeare import TinyShakespeareDataModule
+# from src.data.tiny_shakespeare import TinyShakespeareDataModule
+from src.data.wikitext2 import WikiText2DataModule
 
 def main():
     tb_logger = TensorBoardLogger(save_dir="lightning_logs", name="tb")
@@ -17,7 +18,7 @@ def main():
         def add_arguments_to_parser(self, parser):
             parser.set_defaults({"trainer.logger": [tb_logger, wandb_logger]})
 
-    cli = MyLightningCLI(LitJEPA, TinyShakespeareDataModule, save_config_callback=None)
+    cli = MyLightningCLI(LitJEPA, WikiText2DataModule, save_config_callback=None)
 
 if __name__ == "__main__":
     main()
